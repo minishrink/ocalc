b1fafbc7cd6644a21451996c4031f2a8525f4fde
minishrink <mathur_akanksha@hotmail.com>
2018-09-01 02:24:48 +0100

    Raise Arithmetic_error for undefined/infinity values
    Signed-off-by: minishrink <mathur_akanksha@hotmail.com>

--- src/display.ml
+++ src/display.ml
@@ -11,10 +11,23 @@ let maybe_remove_dot s =
   then String.sub s 0 last
   else s
 
+let handle_float f =
+  let undefined_value () = raise P.(Arithmetic_error "Undefined value") in
+  let infinity_check f =
+    if f=infinity then undefined_value () else f
+  in
+  let check_nan f = match Pervasives.classify_float f with
+    | Pervasives.FP_nan -> undefined_value ()
+    | _ -> f
+  in
+  f |> infinity_check |> check_nan
+
 let display string =
   string
   |> P.interpret
-  |> get_num |> string_of_float |> maybe_remove_dot
+  |> get_num
+  |> handle_float
+  |> string_of_float |> maybe_remove_dot
   |> Printf.sprintf " >> %s"
   |> print_endline
 
